7dcb3c3ff5d7b0b48dd835045647e5728e92fa2c
Catarina Carriço <cdpco@iscte-iul.pt>
2019-04-07 19:20:06 +0100

    create class Main

---
+++ WALLiT-Server/src/main/java/wallit/server/WALLiT_Server/Main.java
@@ -0,0 +1,11 @@
+package wallit.server.WALLiT_Server;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class Main {
+	//@SpringBootApplication
+	public static void main(String[] args) {
+		//SpringApplication.run(Main.class, args);
+	}
+	
+}
--- WALLiT-Server/src/main/java/wallit/server/WALLiT_Server/SpringController.java
+++ WALLiT-Server/src/main/java/wallit/server/WALLiT_Server/SpringController.java
@@ -1,17 +1,9 @@
 package wallit.server.WALLiT_Server;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStream;
-//import utilities.Paths;
-import java.io.OutputStream;
 
 import org.json.JSONObject;
-//import org.apache.tomcat.util.http.fileupload.IOUtils;//(?)
-//import org.apache.commons.io.IOUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -33,81 +25,29 @@ import org.json.simple.parser.ParseException;
  */
 @RestController
 public class SpringController {
-	
-	/**
-	 * Adds the submission sent by the user to the engine, so it can start the algorithm processes. This method accepts inputs from a certain origin only.
-	 * @param submission The submission object, converted directly from the user's submitted JSON.
-	 */
-	//@CrossOrigin(origins = "http://localhost:4200")
-	/*
-	@RequestMapping(method = RequestMethod.POST, value = "/send_problem", consumes = "application/json")
-	public void addSubmission(@RequestBody Submission submission)	{
-		engine.addProblemToQueue(submission);
-	}
-	*/
-	/**
-	 * Adds the submission sent by the user to the @see Engine, so it can start the algorithm processes. This method accepts inputs from any testing origin.
-	 * @param submission The @see Submission object, converted directly from the user's submitted JSON.
-	 */
-	/*
-	@RequestMapping(method = RequestMethod.POST, value = "/send_test_problem")
-	public void addTestSubmission(@RequestBody Submission submission)	{
-		engine.addProblemToQueue(submission);
-	}
-	*/
-	/**
-	 * Sends results to the chart service, displaying this data to the user.
-	 * @param problem The corresponding problem
-	 * @return The displaying information
-	 */
-	/*
-	@CrossOrigin(origins = "http://localhost:4200")
-	@RequestMapping(method = RequestMethod.POST, value = "/dataTestEuro.json")
-	public @ResponseBody ResponseEntity<String> sendResults(@RequestBody String problem) {
-		String jsonTxt;
-		OutputStream out;
-		InputStream in;
-		try {
-			
-			in = new FileInputStream(DEPOSITS_FOLDER.RESULTS_FOLDER+problem+"/"+problem+"_results.json");
-			jsonTxt = IOUtils.toString( is );
-			
-			out = new FileOutputStream("dataTestEuro.json");
-		} catch (FileNotFoundException e) {
-			System.err.println("Cannot open the results json file.");
-			return null;
-		} catch (IOException e) {
-			System.err.println("Cannot open the results json file.");
-			return null;
-		}
-		final HttpHeaders httpHeaders= new HttpHeaders();
-	    httpHeaders.setContentType(MediaType.APPLICATION_JSON);
-	    return new ResponseEntity<String>(jsonTxt, httpHeaders, HttpStatus.OK);
-	}
-	*/
-	@RequestMapping(method = RequestMethod.POST, value = "/receivedAmount", consumes = "application/json")
+
+	@RequestMapping(method = RequestMethod.POST, value = "/receivedValues", consumes = "application/json")
 	public void receivedValues (@RequestBody AppData appData) {
 		
 	}
 	@CrossOrigin(origins = "http://localhost:4200")
-	@RequestMapping(method = RequestMethod.POST, value = "/sendValuesSite", consumes = "application/json")
+	@RequestMapping(method = RequestMethod.POST, value = "/sendValuesSite", consumes = "application/graphic")
 	public @ResponseBody ResponseEntity<String> sendValuesSite (@RequestBody AppData appData) {
-		JSONObject jsonObject = null;
+		JSONObject jsonObject;
 		JSONParser parser = new JSONParser();
-		InputStream in;
 		
 		try {
-			
-			
-			try {
+
 				Object obj = parser.parse(new FileReader("./dataTestEuro.json"));
 				jsonObject =  (JSONObject) obj;
-			} catch (ParseException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-
-            
+				final HttpHeaders httpHeaders= new HttpHeaders();
+			    httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+			    return new ResponseEntity<String>(jsonObject.toString(), httpHeaders, HttpStatus.OK);
+			
+		} catch (ParseException e) {
+			e.printStackTrace();   
+			System.err.println("Cannot open the results json file.");
+			return null;
 		} catch (FileNotFoundException e) {
 			System.err.println("Cannot open the results json file.");
 			return null;
@@ -115,9 +55,7 @@ public class SpringController {
 			System.err.println("Cannot open the results json file.");
 			return null;
 		}
-		final HttpHeaders httpHeaders= new HttpHeaders();
-	    httpHeaders.setContentType(MediaType.APPLICATION_JSON);
-	    return new ResponseEntity<String>(jsonObject.toString(), httpHeaders, HttpStatus.OK);
+		
 	}
 	
 
