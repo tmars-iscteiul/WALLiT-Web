ff665c404c4f384afec90c9db18c3879dba7cf44
rmnte-iscteiulpt <32777900+rmnte-iscteiulpt@users.noreply.github.com>
2019-04-23 21:30:52 +0100

    Fixed execution problem with the main class

--- WALLiT-Server/src/main/java/wallit/server/WALLiT_Server/Main.java
+++ WALLiT-Server/src/main/java/wallit/server/WALLiT_Server/Main.java
@@ -1,11 +1,15 @@
 package wallit.server.WALLiT_Server;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@SpringBootApplication
+@ComponentScan(basePackages={"main, spring"})
 public class Main {
-	//@SpringBootApplication
+	
 	public static void main(String[] args) {
-		//SpringApplication.run(Main.class, args);
+		SpringApplication.run(Main.class, args);
 	}
 	
 }
