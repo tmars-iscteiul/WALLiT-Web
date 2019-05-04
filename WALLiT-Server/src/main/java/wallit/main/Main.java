5c7eafdfc779049682d50864caf2700747e7566e
Catarina Carriço <cdpco@iscte-iul.pt>
2019-05-04 21:06:56 +0100

    [Processing] Read JSON file

--- WALLiT-Server/src/main/java/wallit/main/Main.java
+++ WALLiT-Server/src/main/java/wallit/main/Main.java
@@ -5,16 +5,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 import wallit.javaserver.JavaServer;
+import wallit.springbootserver.SpringbootServer;
 
 @SpringBootApplication
 @ComponentScan(basePackages={"main, spring"})
 public class Main {
 	
-	
-	
 	public static void main(String[] args) {
 		new JavaServer().start();
 		//SpringApplication.run(Main.class, args);
+		
+		new SpringbootServer().run();
 	}
 	
 }
